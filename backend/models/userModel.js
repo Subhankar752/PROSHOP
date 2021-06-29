2b7c9bb25dfe15e1c08cb0f473382de5513b1ca6
Subhankar752 <subhankar.majumdar.752@gmail.com>
2021-06-29 12:33:12 +0530

    commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+const userSchema = mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+
+    },
+    isAdmin: {
+        type:boolean,
+        required: true,
+        default: false
+    },
+} , {
+    timstamps: true
+})
+
+const User = mongoose.model('User' , userSchema)
+export default User
