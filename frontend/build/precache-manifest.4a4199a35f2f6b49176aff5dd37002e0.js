d37e4d502565385d3cfc06dae7c9b3f1530ab5dd
Subhankar752 <subhankar.majumdar.752@gmail.com>
2021-07-13 11:55:56 +0530

    commit

---
+++ frontend/build/precache-manifest.4a4199a35f2f6b49176aff5dd37002e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589021ab18c3253803c631d826733865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a68fc8569199441e555e",
+    "url": "/static/css/main.2a679012.chunk.css"
+  },
+  {
+    "revision": "4ea7726c52bc4613c6ca",
+    "url": "/static/js/2.7303df09.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.7303df09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9a62c0d1b7c9fbf2d0",
+    "url": "/static/js/3.5d78db41.chunk.js"
+  },
+  {
+    "revision": "a68fc8569199441e555e",
+    "url": "/static/js/main.4f262eeb.chunk.js"
+  },
+  {
+    "revision": "f8ec8159f49eae8f2f9f",
+    "url": "/static/js/runtime-main.8626cd54.js"
+  }
+]);
